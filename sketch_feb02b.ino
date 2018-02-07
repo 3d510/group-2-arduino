01f8e24a8dc70c0e5662bb7613463877378ecddd
Doan Duy Duc <3dmailboxes@gmail.com>
2018-02-07 20:12:03 +0800

    testing motor

--- sketch_feb02b.ino
+++ sketch_feb02b.ino
@@ -1,5 +1,18 @@
 #include "DualVNH5019MotorShield.h"
+#include "PinChangeInt.h"
+
 DualVNH5019MotorShield md;
+
+#define MOTOR_L_ENCODER_A 3
+#define MOTOR_L_ENCODER_B 5
+
+#define MOTOR_R_ENCODER_A 11
+#define MOTOR_R_ENCODER_B 13
+
+int motorLAccmEncoderCount = 0;      //Accumulated encoder's ticks count of left motor
+// volatile int motorLNetEncoderCount = 0;       //Net encoder's ticks count of left motor
+int motorRAccmEncoderCount = 0;
+
 void stopIfFault()
 {
   if (md.getM1Fault())
@@ -14,15 +27,33 @@ void stopIfFault()
   }
 }
 
+
+
 void setup()
 {
   Serial.begin(115200);
   Serial.println("Dual VNH5019 Motor Shield");
   md.init();
+
+  pinMode(MOTOR_L_ENCODER_A, INPUT);
+  PCintPort::attachInterrupt(MOTOR_L_ENCODER_A, &motorLISRA, CHANGE);  //Attach left motor encoder interrupt pin to the ISR
+//  PCintPort::attachInterrupt(MOTOR_R_ENCODER_A, &moto/rRISRlA, CHANGE); 
 }
 
 void loop() {
-  md.setSpeeds(400, -400);
+  md.setSpeeds(200, 185);
   stopIfFault();
+//  Serial.println(pulseIn(MOTOR_L_ENCODER_A/, HIGH));
+}
+
+void motorLISRA()      //ISR for left motor encoder interrupt
+{
+    motorLAccmEncoderCount++;
+}
+
+
+void motorRISRA()      //ISR for left motor encoder interrupt
+{
+    motorRAccmEncoderCount++;
 }
 
