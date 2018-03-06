2732ab323dcd606dc83dd2e92998b1bc7f5ff8aa
Doan Duy Duc <3dmailboxes@gmail.com>
2018-03-06 15:22:22 +0800

    add go function

--- main/main.ino
+++ main/main.ino
@@ -2,18 +2,26 @@
 #include "PinChangeInt.h"
 #include <PID_v1.h>
 
-#define TEST_TIME 0.002
-#define FRONT_RIGHT A0
-#define FRONT_CENTER A1
-#define FRONT_LEFT A2
-#define LEFT A3
-#define RIGHT A4
+#define L1 A0
+#define F3 A1
+#define R2 A2
+#define R1 A3
+#define F1 A4
 #define BACK A5 
-#define READ_TIMES 7
-#define encoderRPinA 3
-#define encoderRPinB 5
-#define encoderLPinA 11
-#define encoderLPinB 13
+
+#define TEST_TIME 0.002
+#define READ_TIMES 11
+#define FORWARD 0
+#define ROTATE_CW 1
+#define ROTATE_CCW 2
+
+const int encoderRPinA = 11;
+const int encoderRPinB = 13;
+const int encoderLPinA = 3;
+const int encoderLPinB = 5;
+const double iteration_time = 0.02;
+const double wheel_radius = 3; // cm
+const double robot_radius = 9.5; // cm
 
 volatile int encoderRPos = 0;
 int encoderRPinALast = LOW;
@@ -49,41 +57,126 @@ int leftTickCount[100];
 int rightTickCount[100];
 int tickDiff[100];
 
+int counter = 0;
+int ticks[111];
+
 void loop() {
-  if (!done) {    
-    md.setSpeeds(150, 150);
-    
-    delay(3000);
-    
-    leftTickCount[0] = encoderLPos;
-    rightTickCount[0] = encoderRPos;
-    
-    md.setSpeeds(300, 300);
-    
-    while (count < 100) {
-      //rpms[count] = abs(readRpmWithInterrupt(false, 0.02));
-      leftTickCount[count] = encoderLPos;
-      rightTickCount[count] = encoderRPos;
-      encoderLPos = 0;
-      encoderRPos = 0;
-      delay(5);
-      count++;
+  if (!done) {  
+//    while(1) {
+//      Serial.println(readSingleSensor(L1));
+//      delay(3000);
+//    }
+    int count = 0;
+    while (1) {
+      //if (count == 7) break;
+      
+      goDigitalDist(50, 150, FORWARD, true);
+      delay(1000);
+
+       goDigitalDist(40, 2*PI*robot_radius * 45/360, ROTATE_CW, false); 
+      delay(1000);
+
+      goDigitalDist(40, 50, FORWARD, false);
+      delay(1000);
+
+      goDigitalDist(40, 2*PI*robot_radius * 105/360, ROTATE_CCW, false); 
+      delay(1000);
+
+      goDigitalDist(40, 50, FORWARD, false);
+      delay(1000);
+
+      goDigitalDist(40, 2*PI*robot_radius * 45/360, ROTATE_CW, false); 
+      delay(1000);
+      
+      goDigitalDist(40, 20, FORWARD, true);
+
+      break;
+      
+
+      
+//     
+//      int Time = millis();
+//      
+//      double d1 = readSingleSensor(R2);
+//      double d2 = readSingleSensor(L1);
+//
+//      Serial.println(millis() - Time);
+//      break;
+     
+
+//      Serial.print(d1);
+//      Serial.print(" ");
+//      Serial.println(d2);
+
+      //if ((d1 <= 15 && d1 >= 10) || (d2 <= 15 && d2 >= 10)) break;
+      
     }
+    md.setSpeeds(0,0);
     
-    for (int i = 0; i < count; i++) {
-      Serial.print(i);
-      Serial.print(" "); 
-      Serial.print(leftTickCount[i]);
-      Serial.print(" "); 
-      Serial.println(rightTickCount[i]);
-    }
+    //delay(5000);
+    // goDigital(60, 7, FORWARD);
+    //goDigitalDist(40, 2*PI*robot_radius * (1080 + 15)/360, ROTATE_CW);
+
+    // goDigitalDist(40, 150, FORWARD);
+    
+    //delay(3000);
+    //goDigital(40, 5, FORWARD);
+    //calTimeForward(40, 30)
+
+//      md.setSpeeds(300, 0);
+//
+//      while(1) {
+//        float Time = millis();
+//        if (Time > 10000) break;
+//        encoderLPos = 0;
+//        delay(5);
+//        Serial.println(ticksToRpm(encoderLPos, 0.005));
+//      }
+      
+//    md.setSpeeds(300, 295);
+//    delay(100);
+//    
+//    while (counter < 100) {
+//      encoderLPos = 0;
+//      delay(5);
+//      ticks[counter] = encoderLPos;
+//      counter++;
+//    }
+//
+//    for (int i = 0; i < 100; i++) {
+//      Serial.println(ticksToRpm(ticks[i], 0.005));
+//    }
+    
+//    delay(3000);
+//    
+//    leftTickCount[0] = encoderLPos;
+//    rightTickCount[0] = encoderRPos;
+//    
+//    md.setSpeeds(400, 400);
+//    
+//    while (count < 100) {
+//      //rpms[count] = abs(readRpmWithInterrupt(false, 0.02));
+//      leftTickCount[count] = encoderLPos;
+//      rightTickCount[count] = encoderRPos;
+//      encoderLPos = 0;
+//      encoderRPos = 0;
+//      delay(5);
+//      count++;
+//    }
+//    
+//    for (int i = 0; i < count; i++) {
+//      Serial.print(i);
+//      Serial.print(" "); 
+//      Serial.print(leftTickCount[i]);
+//      Serial.print(" "); 
+//      Serial.println(rightTickCount[i]);
+//    }
     done = true;
 //    goAnalog();
 //    if (millis() >= 2000) done = true;
   } else {
     md.setSpeeds(0,0);
   }
-
 }
 
 float readRpmWithInterrupt(bool isLeftWheel, float delayTime) { // delayTime in seconds
@@ -119,74 +212,225 @@ void doEncoderRight() {
   encoderRPos++;
 }
 
+//---------PID-----------//
+
+double errorPrior = 0, integral = 0;
+int leftPrevTicks = 0, rightPrevTicks = 0;
+
+void goDigital(double desired_rpm, float time, int direction) {  
+  int leftSpeed = 0, rightSpeed = 0;
+  int LMag = 1, RMag = 1;
+
+  if (direction == ROTATE_CW) {
+    RMag = -1;
+  } else if (direction == ROTATE_CCW) {
+    LMag = -1;
+  }
+  
+  leftPrevTicks = encoderLPos;
+  rightPrevTicks = encoderRPos;
+
+  delay(iteration_time * 1000);
+
+  float start_time = millis();
+  
+  while(1) {
+    float Time = millis();
+    if (Time > start_time + 1000 * time) break;
+    
+    double leftDigitalPidOutput = computeDigitalPid(desired_rpm, ticksToRpm(encoderLPos - leftPrevTicks, iteration_time), 7.8, 4.75, 0.08);
+    double rightDigitalPidOutput = computeDigitalPid(desired_rpm, ticksToRpm(encoderRPos - rightPrevTicks, iteration_time), 7.75, 4.5, 0.005);
+
+    double newLeftSpeed = constrain(abs(leftSpeed) + leftDigitalPidOutput, 0 , 400) * LMag;
+    double newRightSpeed = constrain(abs(rightSpeed) + rightDigitalPidOutput, 0 , 400) * RMag;
+    md.setSpeeds(newLeftSpeed, newRightSpeed);
+    //md.setSpeeds(newLeftSpeed, 0);
+    //md.setSpeeds(0, newRightSpeed);
+
+    leftPrevTicks = encoderLPos;
+    rightPrevTicks = encoderRPos;
+    delay(iteration_time * 1000); // iteration_time in seconds
+  }
+  md.setSpeeds(0,0);
+}
+
+void goDigitalDist(double desired_rpm, float dist, int direction, bool sense) {  
+  int leftSpeed = 0, rightSpeed = 0;
+  int LMag = 1, RMag = 1;
+
+  if (direction == ROTATE_CW) {
+    RMag = -1;
+  } else if (direction == ROTATE_CCW) {
+    LMag = -1;
+  }
+  
+  leftPrevTicks = encoderLPos;
+  rightPrevTicks = encoderRPos;
+
+  delay(iteration_time * 1000);
+
+  int totalTicks = (int) (562.25 * dist / (2 * PI * wheel_radius));
+  
+  int start_ticks = encoderLPos;
+  
+  while(1) {
+    if (encoderLPos >= start_ticks + totalTicks) break;
+    
+    double leftDigitalPidOutput = computeDigitalPid(desired_rpm, ticksToRpm(encoderLPos - leftPrevTicks, iteration_time), 7.8, 4.75, 0.08);
+    double rightDigitalPidOutput = computeDigitalPid(desired_rpm, ticksToRpm(encoderRPos - rightPrevTicks, iteration_time), 7.75, 4.5, 0);
+
+    double newLeftSpeed = constrain(abs(leftSpeed) + leftDigitalPidOutput, 0 , 400) * LMag;
+    double newRightSpeed = constrain(abs(rightSpeed) + rightDigitalPidOutput, 0 , 400) * RMag;
+    md.setSpeeds(newLeftSpeed, newRightSpeed);
+    //md.setSpeeds(newLeftSpeed, 0);
+    //md.setSpeeds(0, newRightSpeed);
+    
+    leftPrevTicks = encoderLPos;
+    rightPrevTicks = encoderRPos;
+
+    if (sense) {
+
+    double d1 = readSingleSensor(R2);
+    double d2 = readSingleSensor(L1);
+    
+    if ((d1 <= 13.5 && d1 >= 10) || (d2 <= 13.5 && d2 >= 10)) break;
+    }
+    
+    delay(iteration_time * 1000); // iteration_time in seconds
+  }
+  md.setSpeeds(0,0);
+}
+
+int id = 1;
+double computeDigitalPid(double desired_value, double actual_value, double kp, double ki, double kd) {
+  //Serial.print(millis());
+  //Serial.print(" ");
+  Serial.println(actual_value);
+  double error = desired_value - actual_value;
+  integral += (error * iteration_time);
+  double derivative = (error - errorPrior)/iteration_time;
+  double output = kp * error + ki * integral + kd * derivative;
+  errorPrior = error;
+  //Serial.println(output);
+  return output;
+}
+
+double ticksToRpm(int tickCount, double period) { // period in seconds
+  return 60 / (562.25 / ((tickCount) / period));
+}
+
+double calTimeForward(double rpm, double distance) { // distance in cm
+  return (distance - 9.7) / (2 * PI * wheel_radius * rpm / 60) + 0.667;
+}
+
+double calTimeRotate(double rpm, double angle) { // angle in degree
+
+  return (2 * PI * robot_radius * angle / 360 - 9.7) / (2 * PI * wheel_radius * rpm / 60) + 0.667;
+}
+
+//------------Sensor--------------//
+
 float readSingleSensor(int sensorNumber) {
   // read the pin 7 times to get median value
   int sensorVal[READ_TIMES];
   int sortedSensorVal[READ_TIMES];
   for (int i = 0; i < READ_TIMES; i++) {
-    sensorVal[i] = analogRead(sensorNumber);         
-    delay(10);
+    sensorVal[i] = analogRead(sensorNumber);   
+    //Serial.println(sensorVal[i]);      
+    //delay(10);
   }
-  sortedSensorVal[0] = sensorVal[0];
-  for (int i = 1; i < READ_TIMES; i++) {
-    int id = 0;
-    while (id < i && sortedSensorVal[id] <= sensorVal[i])
-      id++;
-    if (id == i) {
-      sortedSensorVal[i] = sensorVal[i];
-    } else {
-      for (int j = i - 1; j >= id; j--) {
-        sortedSensorVal[j+1] = sensorVal[j]; 
-      } 
-      sortedSensorVal[id] = sensorVal[i]; 
-    }
-  }
-  int readValue = sortedSensorVal[READ_TIMES/2];
+  
+//  sortedSensorVal[0] = sensorVal[0];
+//  for (int i = 1; i < READ_TIMES; i++) {
+//    int id = 0;
+//    while (id < i && sortedSensorVal[id] <= sensorVal[i])
+//      id++;
+//    if (id == i) {
+//      sortedSensorVal[i] = sensorVal[i];
+//    } else {
+//      for (int j = i - 1; j >= id; j--) {
+//        sortedSensorVal[j+1] = sensorVal[j]; 
+//      } 
+//      sortedSensorVal[id] = sensorVal[i]; 
+//    }
+//  }
+  
+//  int readValue = sortedSensorVal[READ_TIMES/2];
+
+  int readValue = kthSmallest(sensorVal, 0 ,READ_TIMES-1, READ_TIMES/2);
 
   // deduce distance from value read from analog pin
   switch (sensorNumber) {
-    case FRONT_RIGHT: 
-      return 8544.11/readValue - 7.29;
-    case FRONT_LEFT: 
-      return 8068.98/readValue - 5.63;
-    case LEFT:
-      return 8766.22/readValue - 7.35;
-    case RIGHT:
-      return 7904.38/readValue - 5.62;
+    case F1: 
+      return 5452.08/(readValue - 3) - 1.246;
+    case R1: 
+      return 6534.06/(readValue - 3) - 3.83;
+    case L1:
+      return 6270.15/(readValue - 3) - 2.8977;
+    case F3:
+      return 14054.34/(readValue - 3) - 5.472;
     case BACK:
-      return 8068.41/readValue - 6.77;
+      return 6142.70/(readValue - 3) - 3.088;
+    case R2:
+      return 6725.72 / (readValue - 3) - 4.077;
+//      case R2:
+//        return 6787 / (readValue - 3) - 4;
   }
-  
 }
 
-
-
-
-void goAnalog() {
-  int m1Encoder = readTicksWithInterrupt(true, 0.005);
-  int m2Encoder = readTicksWithInterrupt(false, 0.02);
-  Serial.println(readRpmWithInterrupt(true, 0.02));
-  Serial.println(readRpmWithInterrupt(false, 0.02));
-  int output = computeAnalogPid(m1Encoder, m2Encoder);
-  md.setSpeeds(300 - output, 295 + output);
+int partition(int arr[], int l, int r)
+{
+    int x = arr[r], i = l;
+    for (int j = l; j <= r - 1; j++) {
+        if (arr[j] <= x) {
+            swap(&arr[i], &arr[j]);
+            i++;
+        }
+    }
+    swap(&arr[i], &arr[r]);
+    return i;
 }
 
-int computeAnalogPid(int leftTicks, int rightTicks) {
-    int error, pwm1 = 255, pwm2 = 255;
-    float integral, derivative, output;
-    float Kp = 0.75;  //0-1
-    float Kd = 1.65;  //1-2
-    float Ki = 0.75;  //0.5-1
-
-    error = leftTicks - rightTicks;
-    integral += error;
-    derivative = error - prevError;
-    output = Kp * error + Ki * integral + Kd * derivative;
-    prevError = error;
-
-    pwm1 = output;
-    return pwm1;
+void swap(int* a, int* b) {
+  int tmp = *a;
+  *a = *b;
+  *b = tmp;
 }
+ 
+// This function returns k'th smallest 
+// element in arr[l..r] using QuickSort 
+// based method.  ASSUMPTION: ALL ELEMENTS
+// IN ARR[] ARE DISTINCT
+int kthSmallest(int arr[], int l, int r, int k)
+{
+    // If k is smaller than number of 
+    // elements in array
+    if (k > 0 && k <= r - l + 1) {
+ 
+        // Partition the array around last 
+        // element and get position of pivot 
+        // element in sorted array
+        int index = partition(arr, l, r);
+ 
+        // If position is same as k
+        if (index - l == k - 1)
+            return arr[index];
+ 
+        // If position is more, recur 
+        // for left subarray
+        if (index - l > k - 1) 
+            return kthSmallest(arr, l, index - 1, k);
+ 
+        // Else recur for right subarray
+        return kthSmallest(arr, index + 1, r, 
+                            k - index + l - 1);
+    }
+ 
+    // If k is more than number of 
+    // elements in array
+    return 1000;
+}
+
 
 
 
